b3c41170cbc2a21632b9d949722a311d3675309b
Parwaiz Alam <parwez.inf77@gmail.com>
2024-07-15 21:56:05 +0530

    changed model

--- backend/dist/models/Price.js
+++ backend/dist/models/Price.js
@@ -1,33 +1,24 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importStar(require("mongoose"));
-const PriceSchema = new mongoose_1.Schema({
-    symbol: { type: String, required: true },
-    price: { type: Number, required: true },
-    timestamp: { type: Date, default: Date.now }
+const mongoose_1 = __importDefault(require("mongoose"));
+const PriceSchema = new mongoose_1.default.Schema({
+    symbol: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    }
+}, {
+    timestamps: true
 });
-exports.default = mongoose_1.default.model('Price', PriceSchema);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUHJpY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9tb2RlbHMvUHJpY2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUFBLHFEQUFzRDtBQVF0RCxNQUFNLFdBQVcsR0FBVyxJQUFJLGlCQUFNLENBQUM7SUFDckMsTUFBTSxFQUFFLEVBQUUsSUFBSSxFQUFFLE1BQU0sRUFBRSxRQUFRLEVBQUUsSUFBSSxFQUFFO0lBQ3hDLEtBQUssRUFBRSxFQUFFLElBQUksRUFBRSxNQUFNLEVBQUUsUUFBUSxFQUFFLElBQUksRUFBRTtJQUN2QyxTQUFTLEVBQUUsRUFBRSxJQUFJLEVBQUUsSUFBSSxFQUFFLE9BQU8sRUFBRSxJQUFJLENBQUMsR0FBRyxFQUFFO0NBQzdDLENBQUMsQ0FBQztBQUVILGtCQUFlLGtCQUFRLENBQUMsS0FBSyxDQUFTLE9BQU8sRUFBRSxXQUFXLENBQUMsQ0FBQyJ9
+exports.default = mongoose_1.default.model('prices', PriceSchema);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUHJpY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9tb2RlbHMvUHJpY2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSx3REFBZ0M7QUFFaEMsTUFBTSxXQUFXLEdBQUcsSUFBSSxrQkFBUSxDQUFDLE1BQU0sQ0FBQztJQUNwQyxNQUFNLEVBQUU7UUFDSixJQUFJLEVBQUUsTUFBTTtRQUNaLFFBQVEsRUFBRSxJQUFJO0tBQ2pCO0lBQ0QsS0FBSyxFQUFFO1FBQ0gsSUFBSSxFQUFFLE1BQU07UUFDWixRQUFRLEVBQUUsSUFBSTtLQUNqQjtJQUNELFNBQVMsRUFBRTtRQUNQLElBQUksRUFBRSxJQUFJO1FBQ1YsT0FBTyxFQUFFLElBQUksQ0FBQyxHQUFHO0tBQ3BCO0NBQ0osRUFBQztJQUNFLFVBQVUsRUFBRSxJQUFJO0NBQ25CLENBQUMsQ0FBQTtBQUVGLGtCQUFlLGtCQUFRLENBQUMsS0FBSyxDQUFDLFFBQVEsRUFBQyxXQUFXLENBQUMsQ0FBQSJ9
--- backend/models/Price.ts
+++ backend/models/Price.ts
@@ -1,21 +1,20 @@
-import mongoose, { Document, Schema, Model } from 'mongoose';
+import mongoose from "mongoose";
 
-export interface IPrice extends Document {
-  symbol: string;
-  price: number;
-  timestamp: Date;
-}
+const PriceSchema = new mongoose.Schema({
+    symbol: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    }
+},{
+    timestamps: true
+})
 
-const PriceSchema: Schema = new Schema({
-  symbol: { type: String, required: true },
-  price: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now }
-});
-
-
-export interface IPriceModel extends Model<IPrice> {}
-
-export const Price: IPriceModel =
-  mongoose.models.Price || mongoose.model<IPrice>('Price', PriceSchema);
-
-export default Price;
+export default mongoose.model('prices',PriceSchema)
